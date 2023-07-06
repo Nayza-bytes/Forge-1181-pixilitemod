02e6ca4d551ee1e292ac65a603fc69c543abb7bc
Nayza-Bytes <quetru123456789@gmail.com>
2023-07-06 22:25:46 +0200

    Added ModItems.java + Registered my first item

---
+++ src/main/java/net/nayza/pixilitemod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.nayza.pixilitemod.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nayza.pixilitemod.PixiliteMod;
+
+public class ModItems {
+    //DeferredRegister is to tell to forge that all of our items are from our mod
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, PixiliteMod.MOD_ID);
+
+    //Method to register my first item
+    public static final RegistryObject<Item> PIXILITE_CRYSTAL = ITEMS.register("pixilite_crystal",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+
+}
